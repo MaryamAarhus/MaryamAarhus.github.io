7b2b9fddda52a7ea9fa62a2c1bfd486edcae767f
Maryam Homayounzadeh <maryamhomayounzadeh@gmail.com>
2024-01-13 18:02:43 +0100

    QuizApp questions added.

---
+++ data/questions.js
@@ -0,0 +1,202 @@
+let quizQuestions = [
+    {
+        question: "He argues that dreams that repeat often have an important message, signaling the dreamer to get over ….. fear or solve a problematic issue.",
+        options: [
+            { text: "any", isCorrect: false },
+            { text: "some", isCorrect: true },
+            { text: "many", isCorrect: false },
+            { text: "few", isCorrect: false }               
+        ]
+    },
+    {
+        question: "The ancient Hopewell people of North America probably cultivated corn and …… crops, but hunting and gathering were still of critical importance in their economy.",
+        options: [
+            { text: "another", isCorrect: false },
+            { text: "the others", isCorrect: false },
+            { text: "other", isCorrect: true },
+            { text: "other than", isCorrect: false }
+        ]
+    },
+    
+    {
+        question: "The wallet inside the car ------ him to steal the car.",
+        options: [
+            { text: "inspired", isCorrect: false },
+            { text: "tempted", isCorrect: true },
+            { text: "persuated", isCorrect: false },
+            { text: "exposed", isCorrect: false }
+        ]
+    },
+
+    {
+        question: "It was beautiful when all the fireworks lit up the sky ------- and formed the shape of our countrys flag.",
+        options: [
+            { text: "simultaneously", isCorrect: true },
+            { text: "excitedly", isCorrect: false },
+            { text: "belatedly", isCorrect: false },
+            { text: "immediately", isCorrect: false }
+        ]
+    },
+
+    {
+        question: "Try not to do anything without considering its --------.",
+        options: [
+            { text: "B.	consequences", isCorrect: true },
+            { text: "relation", isCorrect: false },
+            { text: "commitments", isCorrect: false },
+            { text: "capacity", isCorrect: false }
+        ]
+    },
+
+    {
+        question: "How can we ………. the barriers of fear and hostility which divide the two communities?",
+        options: [
+            { text: "obstruct", isCorrect: true },
+            { text: "abandon", isCorrect: false },
+            { text: "put out", isCorrect: false },
+            { text: "wearout", isCorrect: true },
+        ]
+    },
+
+    {
+        question: "Some scientists ………. that heat deep inside our planet causes material to expand and move upwards.",
+        options: [
+           { text: "apprehend", isCorrect: true }, 
+           { text: "explicate", isCorrect: false },
+           { text: "maintain", isCorrect: false },
+           { text: "acclaim", isCorrect: false },
+        ]
+    },
+
+    {
+        question: "The documentary showed an eye operation in ……… detail.",
+        options: [
+            { text: "feasible", isCorrect: false }, 
+            { text: "vital", isCorrect: false },
+            { text: "intricate", isCorrect: false }, 
+            { text: "minute", isCorrect: true }
+        ]
+    },
+
+    {
+        question: "The house is ……. toward sun, so it gets lots of sun.",
+        options: [
+            { text: "detected", isCorrect: false },
+            { text: "oriented", isCorrect: true },
+            { text: "dispersed", isCorrect: false },
+            { text: "settled", isCorrect: false }
+        ]
+    },
+
+    {
+         question: "To increase the number of female representatives, the committee decided to …….. in favor of women for three years.",
+        options: [
+            { text: "distinguish", isCorrect: false },
+            { text: "discriminate", isCorrect: true },
+            { text: "caution", isCorrect: false },
+            { text: "appeal", isCorrect: false }
+        ]
+    },
+
+    {
+        question: "The rules regarding attendance and homework are …….. in that institute; there is no room for any disobedience.",
+        options: [
+            { text: "robust", isCorrect: false },
+            { text: "limber", isCorrect: false },
+            { text: "rigid", isCorrect: true },
+            { text: "accurate", isCorrect: false }
+        ]
+    },
+
+    {
+       question: "The …… earthquake left a lot of people homeless.",
+        options: [
+            { text: "dramatic", isCorrect: false },
+            { text: "dormant", isCorrect: false },
+            { text: "rudimentary", isCorrect: false },
+            { text: "devastating", isCorrect: true }
+        ] 
+    },
+
+    {
+       question: "The newspaper reports no …….. progress in the negotiations.",
+        options: [
+            { text: "exceptional", isCorrect: false },
+            { text: "discernible", isCorrect: true },
+            { text: "solid", isCorrect: false },
+            { text: "negligible", isCorrect: false }
+        ]  
+    },
+
+    {
+       question: "I believe that sooner or later good must ……. over evil.",
+        options: [
+            { text: "overcome", isCorrect: false },
+            { text: "triumph", isCorrect: true },
+            { text: "terminate", isCorrect: false },
+            { text: "emphasize", isCorrect: false }
+        ] 
+    },
+
+    {
+      question: "These plants are particularly ………. to frost.",
+        options: [
+            { text: "suspicious", isCorrect: false },
+            { text: "suitable", isCorrect: false },
+            { text: "inaccessible", isCorrect: false },
+            { text: "susceptible", isCorrect: true }
+        ]  
+    },
+
+    {
+        question: "Many experts …… rewarding your child for good behavior.",
+        options: [
+            { text: "claim", isCorrect: false },
+            { text: "legitimize", isCorrect: false },
+            { text: "sustain", isCorrect: false },
+            { text: "advocate", isCorrect: true }
+        ]
+    },
+
+    {
+        question: "Going to a school for gifted students helped him to …….. as a musical prodigy.",
+        options: [
+            { text: "thrive", isCorrect: true },
+            { text: "elude", isCorrect: false },
+            { text: "promote", isCorrect: false },
+            { text: "defy", isCorrect: false }
+        ]
+    },
+
+    {
+      question: "Our toys are designed to ……. the rough treatment of the average five-year-old.",
+        options: [
+            { text: "overlook", isCorrect: false },
+            { text: "magnify", isCorrect: false },
+            { text: "protect", isCorrect: false },
+            { text: "withstand", isCorrect: true }
+        ]  
+    },
+
+    {
+      question: "I wonder how many celebrities actually use the products they ……….",
+        options: [
+            { text: "emerge", isCorrect: false },
+            { text: "thrive", isCorrect: false },
+            { text: "exaggerate", isCorrect: false },
+            { text: "endorse", isCorrect: true }
+        ]   
+    },
+
+    {
+       question: "Her behavior was a/an …….. mixture of the sophisticated and the childlike.",
+        options: [
+            { text: "intricate", isCorrect: false },
+            { text: "peculiar", isCorrect: true },
+            { text: "insignificant", isCorrect: false },
+            { text: "scattered", isCorrect: false }
+        ] 
+    }
+
+
+];
